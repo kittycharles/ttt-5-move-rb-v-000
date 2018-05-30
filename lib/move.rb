486b900dbe50bb9c3f7a9d88f04e44035cde2ea0
Richard Sanchez <richard.charles.s@gmail.com>
2018-05-30 23:49:34 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -4,7 +4,6 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  return board
 end
 
 # code your input_to_index and move method here!
@@ -14,5 +13,5 @@ end
 
 def move(board, index, character = "X")
   board[0] = "X"
-  board[4] = "O"
+  return board[4] = "O"
 end
