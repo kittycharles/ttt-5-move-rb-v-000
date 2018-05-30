65d64993eb9d40d34dcb2e2d9704f70ae95f5e2f
Richard Sanchez <richard.charles.s@gmail.com>
2018-05-30 22:43:47 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,6 +15,7 @@ def move(board, index, character = "X")
   def update_array_at_with(array, index, value)
     array[index] = value
   end
-
+  update_array_at_with(board, 0, "X")
+  return board 
   update_array_at_with(board, 4, "0")
 end
